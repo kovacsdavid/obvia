0ced7b954f18817a0131af9be12916bffb6ee3be
Kovács Dávid <kapcsolat@kovacsdavid.dev>
2026-05-16 09:02:32 +0200

    feat(tasks): print backend
    Signed-off-by: Kovács Dávid <kapcsolat@kovacsdavid.dev>

--- backend/src/tenant/tasks/model.rs
+++ backend/src/tenant/tasks/model.rs
@@ -18,6 +18,7 @@
  */
 use bigdecimal::BigDecimal;
 use chrono::{DateTime, Local};
+use indexmap::IndexMap;
 use serde::{Deserialize, Serialize};
 use sqlx::FromRow;
 use uuid::Uuid;
@@ -62,4 +63,112 @@ pub struct TaskResolved {
     pub updated_at: DateTime<Local>,
     pub deleted_at: Option<DateTime<Local>>,
     pub description: Option<String>,
+}
+
+impl From<TaskResolved> for IndexMap<String, String> {
+    fn from(value: TaskResolved) -> Self {
+        let mut map = IndexMap::new();
+
+        map.insert("id".to_string(), value.id.to_string());
+        map.insert("worksheet_id".to_string(), value.worksheet_id.to_string());
+        map.insert("worksheet".to_string(), value.worksheet);
+        map.insert("service_id".to_string(), value.service_id.to_string());
+        map.insert("service".to_string(), value.service);
+        map.insert("currency_code".to_string(), value.currency_code);
+        map.insert(
+            "quantity".to_string(),
+            value.quantity.map(|v| v.to_string()).unwrap_or_default(),
+        );
+        map.insert(
+            "price".to_string(),
+            value.price.map(|v| v.to_string()).unwrap_or_default(),
+        );
+        map.insert("tax_id".to_string(), value.tax_id.to_string());
+        map.insert("tax".to_string(), value.tax);
+        map.insert("created_by_id".to_string(), value.created_by_id.to_string());
+        map.insert("created_by".to_string(), value.created_by);
+        map.insert("status".to_string(), value.status);
+        map.insert("priority".to_string(), value.priority.unwrap_or_default());
+        map.insert(
+            "due_date".to_string(),
+            value.due_date.map(|dt| dt.to_rfc3339()).unwrap_or_default(),
+        );
+        map.insert("created_at".to_string(), value.created_at.to_rfc3339());
+        map.insert("updated_at".to_string(), value.updated_at.to_rfc3339());
+        map.insert(
+            "deleted_at".to_string(),
+            value
+                .deleted_at
+                .map(|dt| dt.to_rfc3339())
+                .unwrap_or_default(),
+        );
+        map.insert(
+            "description".to_string(),
+            value.description.unwrap_or_default(),
+        );
+
+        map
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn task_resolved_into_index_map() {
+        let id = Uuid::new_v4();
+        let worksheet_id = Uuid::new_v4();
+        let service_id = Uuid::new_v4();
+        let tax_id = Uuid::new_v4();
+        let created_by_id = Uuid::new_v4();
+        let date_test = Local::now();
+        let mut expected = IndexMap::new();
+
+        expected.insert("id".to_string(), id.to_string());
+        expected.insert("worksheet_id".to_string(), worksheet_id.to_string());
+        expected.insert("worksheet".to_string(), "test1".to_string());
+        expected.insert("service_id".to_string(), service_id.to_string());
+        expected.insert("service".to_string(), "test2".to_string());
+        expected.insert("currency_code".to_string(), "test3".to_string());
+        expected.insert("quantity".to_string(), "10".parse().unwrap());
+        expected.insert("price".to_string(), "20".parse().unwrap());
+        expected.insert("tax_id".to_string(), tax_id.to_string());
+        expected.insert("tax".to_string(), "test4".to_string());
+        expected.insert("created_by_id".to_string(), created_by_id.to_string());
+        expected.insert("created_by".to_string(), "test5".to_string());
+        expected.insert("status".to_string(), "test6".to_string());
+        expected.insert("priority".to_string(), "test7".to_string());
+        expected.insert("due_date".to_string(), date_test.to_rfc3339());
+        expected.insert("created_at".to_string(), date_test.to_rfc3339());
+        expected.insert("updated_at".to_string(), date_test.to_rfc3339());
+        expected.insert("deleted_at".to_string(), "".to_string());
+        expected.insert("description".to_string(), "test8".to_string());
+
+        let input: IndexMap<String, String> = TaskResolved {
+            id,
+            worksheet_id,
+            worksheet: "test1".to_string(),
+            service_id,
+            service: "test2".to_string(),
+            currency_code: "test3".to_string(),
+            quantity: Some("10".parse().unwrap()),
+            price: Some("20".parse().unwrap()),
+            tax_id,
+            tax: "test4".to_string(),
+            created_by_id,
+            created_by: "test5".to_string(),
+            status: "test6".to_string(),
+            priority: Some("test7".to_string()),
+            due_date: Some(date_test),
+            created_at: date_test,
+            updated_at: date_test,
+            deleted_at: None,
+            description: Some("test8".to_string()),
+        }
+        .into();
+
+        assert_eq!(input, expected);
+    }
 }
--- backend/src/tenant/tasks/service.rs
+++ backend/src/tenant/tasks/service.rs
@@ -21,6 +21,8 @@ use crate::common::MailTransporter;
 use crate::common::dto::{GeneralError, PaginatorMeta, UuidParam};
 use crate::common::error::{FriendlyError, IntoFriendlyError, RepositoryError};
 use crate::common::model::SelectOption;
+use crate::common::pdf::index_map_key_prefix;
+use crate::common::pdf::{PdfGenError, PdfTemplates, gen_pdf_temporary};
 use crate::common::query_parser::GetQuery;
 use crate::manager::auth::dto::claims::Claims;
 use crate::tenant::tasks::TasksModule;
@@ -29,7 +31,9 @@ use crate::tenant::tasks::model::{Task, TaskResolved};
 use crate::tenant::tasks::repository::TasksRepository;
 use crate::tenant::tasks::types::task::{TaskFilterBy, TaskOrderBy};
 use async_trait::async_trait;
+use axum::body::Bytes;
 use axum::http::StatusCode;
+use indexmap::IndexMap;
 use std::str::FromStr;
 use std::sync::Arc;
 use thiserror::Error;
@@ -45,6 +49,9 @@ pub enum TasksServiceError {
 
     #[error("A lista nem létezik")]
     InvalidSelectList,
+
+    #[error("PdfGen error: {0}")]
+    PdfGenError(#[from] PdfGenError),
 }
 
 #[async_trait]
@@ -219,4 +226,17 @@ pub async fn get_paged_list(
                 .ok_or(TasksServiceError::Unauthorized)?,
         )
         .await?)
+}
+
+pub async fn print(
+    claims: &Claims,
+    payload: &UuidParam,
+    repo: Arc<dyn TasksRepository>,
+) -> TasksServiceResult<Bytes> {
+    let params: IndexMap<String, String> = get_resolved_by_id(claims, payload, repo).await?.into();
+    let params = index_map_key_prefix("tasks_resolved", params);
+    Ok(Bytes::from(gen_pdf_temporary(
+        &PdfTemplates::CustomerView,
+        &params,
+    )?))
 }

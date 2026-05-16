13918e12eeaed8b3c2471b7b408840ea4b1a0607
Kovács Dávid <kapcsolat@kovacsdavid.dev>
2026-05-16 11:08:39 +0200

    feat(worksheets): print backend
    Signed-off-by: Kovács Dávid <kapcsolat@kovacsdavid.dev>

--- backend/src/tenant/worksheets/model.rs
+++ backend/src/tenant/worksheets/model.rs
@@ -18,6 +18,7 @@
  */
 use bigdecimal::BigDecimal;
 use chrono::{DateTime, Local};
+use indexmap::IndexMap;
 use serde::{Deserialize, Serialize};
 use sqlx::FromRow;
 use uuid::Uuid;
@@ -55,4 +56,108 @@ pub struct WorksheetResolved {
     pub gross_material_cost: BigDecimal,
     pub net_work_cost: BigDecimal,
     pub gross_work_cost: BigDecimal,
+}
+
+impl From<WorksheetResolved> for IndexMap<String, String> {
+    fn from(value: WorksheetResolved) -> Self {
+        let mut map = IndexMap::new();
+
+        map.insert("id".to_string(), value.id.to_string());
+        map.insert("name".to_string(), value.name);
+        map.insert(
+            "description".to_string(),
+            value.description.unwrap_or_default(),
+        );
+        map.insert("customer_id".to_string(), value.customer_id.to_string());
+        map.insert("customer".to_string(), value.customer);
+        map.insert(
+            "project_id".to_string(),
+            value.project_id.map(|v| v.to_string()).unwrap_or_default(),
+        );
+        map.insert("project".to_string(), value.project.unwrap_or_default());
+        map.insert("created_by_id".to_string(), value.created_by_id.to_string());
+        map.insert("created_by".to_string(), value.created_by);
+        map.insert("status".to_string(), value.status);
+        map.insert("created_at".to_string(), value.created_at.to_rfc3339());
+        map.insert("updated_at".to_string(), value.created_at.to_rfc3339());
+        map.insert(
+            "deleted_at".to_string(),
+            value
+                .deleted_at
+                .map(|dt| dt.to_rfc3339())
+                .unwrap_or_default(),
+        );
+        map.insert(
+            "net_material_cost".to_string(),
+            value.net_material_cost.to_string(),
+        );
+        map.insert(
+            "gross_material_cost".to_string(),
+            value.gross_material_cost.to_string(),
+        );
+        map.insert("net_work_cost".to_string(), value.net_work_cost.to_string());
+        map.insert(
+            "gross_work_cost".to_string(),
+            value.gross_work_cost.to_string(),
+        );
+
+        map
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn worksheet_resolved_into_index_map() {
+        let id = Uuid::new_v4();
+        let customer_id = Uuid::new_v4();
+        let project_id = Uuid::new_v4();
+        let created_by_id = Uuid::new_v4();
+        let date_test = Local::now();
+        let mut expected = IndexMap::new();
+
+        expected.insert("id".to_string(), id.to_string());
+        expected.insert("name".to_string(), "test1".to_string());
+        expected.insert("description".to_string(), "test2".to_string());
+        expected.insert("customer_id".to_string(), customer_id.to_string());
+        expected.insert("customer".to_string(), "test3".to_string());
+        expected.insert("project_id".to_string(), project_id.to_string());
+        expected.insert("project".to_string(), "test4".to_string());
+        expected.insert("created_by_id".to_string(), created_by_id.to_string());
+        expected.insert("created_by".to_string(), "test5".to_string());
+        expected.insert("status".to_string(), "test6".to_string());
+        expected.insert("created_at".to_string(), date_test.to_rfc3339());
+        expected.insert("updated_at".to_string(), date_test.to_rfc3339());
+        expected.insert("deleted_at".to_string(), "".to_string());
+        expected.insert("net_material_cost".to_string(), "10".to_string());
+        expected.insert("gross_material_cost".to_string(), "20".to_string());
+        expected.insert("net_work_cost".to_string(), "30".to_string());
+        expected.insert("gross_work_cost".to_string(), "40".to_string());
+
+        let input: IndexMap<String, String> = WorksheetResolved {
+            id,
+            name: "test1".to_string(),
+            description: Some("test2".to_string()),
+            customer_id,
+            customer: "test3".to_string(),
+            project_id: Some(project_id),
+            project: Some("test4".to_string()),
+            created_by_id,
+            created_by: "test5".to_string(),
+            status: "test6".to_string(),
+            created_at: date_test,
+            updated_at: date_test,
+            deleted_at: None,
+            net_material_cost: "10".parse().unwrap(),
+            gross_material_cost: "20".parse().unwrap(),
+            net_work_cost: "30".parse().unwrap(),
+            gross_work_cost: "40".parse().unwrap(),
+        }
+        .into();
+
+        assert_eq!(input, expected);
+    }
 }
--- backend/src/tenant/worksheets/service.rs
+++ backend/src/tenant/worksheets/service.rs
@@ -21,6 +21,8 @@ use crate::common::MailTransporter;
 use crate::common::dto::{GeneralError, PaginatorMeta, UuidParam};
 use crate::common::error::{FriendlyError, IntoFriendlyError, RepositoryError};
 use crate::common::model::SelectOption;
+use crate::common::pdf::index_map_key_prefix;
+use crate::common::pdf::{PdfGenError, PdfTemplates, gen_pdf_temporary};
 use crate::common::query_parser::GetQuery;
 use crate::manager::auth::dto::claims::Claims;
 use crate::tenant::worksheets::WorksheetsModule;
@@ -29,7 +31,9 @@ use crate::tenant::worksheets::model::{Worksheet, WorksheetResolved};
 use crate::tenant::worksheets::repository::WorksheetsRepository;
 use crate::tenant::worksheets::types::worksheet::{WorksheetFilterBy, WorksheetOrderBy};
 use async_trait::async_trait;
+use axum::body::Bytes;
 use axum::http::StatusCode;
+use indexmap::IndexMap;
 use std::str::FromStr;
 use std::sync::Arc;
 use thiserror::Error;
@@ -45,6 +49,9 @@ pub enum WorksheetsServiceError {
 
     #[error("A lista nem létezik")]
     InvalidSelectList,
+
+    #[error("PdfGen error: {0}")]
+    PdfGenError(#[from] PdfGenError),
 }
 
 #[async_trait]
@@ -195,4 +202,17 @@ pub async fn get_paged_list(
                 .ok_or(WorksheetsServiceError::Unauthorized)?,
         )
         .await?)
+}
+
+pub async fn print(
+    claims: &Claims,
+    payload: &UuidParam,
+    repo: Arc<dyn WorksheetsRepository>,
+) -> WorksheetsServiceResult<Bytes> {
+    let params: IndexMap<String, String> = get_resolved_by_id(claims, payload, repo).await?.into();
+    let params = index_map_key_prefix("worksheet_resolved", params);
+    Ok(Bytes::from(gen_pdf_temporary(
+        &PdfTemplates::CustomerView,
+        &params,
+    )?))
 }
